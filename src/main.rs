0fc67c8f0fd7db913b98834116fccca48d2c290e
Richard Zak <richard.j.zak@gmail.com>
2023-11-15 22:04:52 -0500

    feat: multi-threading
    Signed-off-by: Richard Zak <richard.j.zak@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 use clap::Parser;
 use ipnet::IpNet;
 use std::net::{Shutdown, TcpStream};
+use std::thread;
 
 /// Simple port scanner
 #[derive(Parser)]
 struct Args {
+    /// Maximum number of threads, default is the number of CPUs on the system
+    #[arg(short, long, default_value_t = num_cpus::get())]
+    threads: usize,
+
     /// IP addresses to scan
     pub net: IpNet,
 }
-fn main() {
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    for ip in args.net.hosts() {
-        for port in 1..65535u16 {
-            let server = format!("{ip}:{port}");
+    let (sender, receiver) = crossbeam_channel::bounded(args.threads);
+    let mut threads = Vec::with_capacity(args.threads);
+
+    for _ in 0..args.threads {
+        let recr = receiver.clone();
+        threads.push(thread::spawn(move || loop {
+            if recr.is_empty() {
+                break;
+            }
+            let server = recr.recv().unwrap();
             let stream = match TcpStream::connect(&server) {
                 Ok(s) => s,
                 Err(_) => {
@@ -22,6 +34,19 @@ fn main() {
             };
             println!("{server}");
             stream.shutdown(Shutdown::Both).unwrap_or_default();
+        }));
+    }
+
+    for ip in args.net.hosts() {
+        for port in 1..65535u16 {
+            let server = format!("{ip}:{port}");
+            sender.send(server)?;
         }
     }
+
+    for thread in threads {
+        thread.join().expect("failed to join() thread");
+    }
+
+    Ok(())
 }
